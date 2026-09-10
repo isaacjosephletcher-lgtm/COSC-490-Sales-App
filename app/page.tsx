49d1950051f2bb99a07460282b96c0ba4ae7171b
Isaac Letcher <isaacjosephletcher@gmail.com>
2026-09-10 17:14:44 -0500

    page error test

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,17 @@ export default async function Page() {
   const cookieStore = await cookies()
   const supabase = createClient(cookieStore)
 
-  const { data: todos } = await supabase.from('todos').select()
+  const { data: todos, error } = await supabase.from('todos').select()
 
   return (
-    <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
-      ))}
-    </ul>
+    <div>
+      {error && <pre>Error: {JSON.stringify(error, null, 2)}</pre>}
+      {!error && (!todos || todos.length === 0) && <p>No todos found (query succeeded, but returned 0 rows)</p>}
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
